6f7f6401f5e683da480c677d67a9f51bf3e7bbb3
ahmedlatif1084 <ahmed@repairdesk.co>
2022-04-23 18:01:43 +0500

    working on nest js

---
+++ src/modules/quiz/quiz.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('quizez')
+export class Quiz extends BaseEntity {
+  @PrimaryGeneratedColumn({
+    comment: 'The quiz unique identifier',
+  })
+  id: number;
+  @Column({
+    type: 'varchar',
+  })
+  title: string;
+  @Column({
+    type: 'text',
+  })
+  description: string;
+}
